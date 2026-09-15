8c3a04d519bedf3f4c467eec59f210f90e349a51
ChipButt <jameschipbutt@hotmail.com>
2026-09-15 13:08:30 +0100

    Update offline cache for private storage build

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE='chipin-hq-v2';
-const ASSETS=['./','./index.html','./styles.css','./app1.js','./app2_1.js','./app2_2.js','./app2_3.js','./app2_4.js','./app3.js','./app4.js','./app5.js','./manifest.webmanifest','./chip_in_logo_TRUE_TRANSPARENT.png'];
+const CACHE='chipin-hq-v3';
+const ASSETS=['./','./index.html','./styles.css','./app1.js','./app2_1.js','./app2_2.js','./app2_3.js','./app2_4.js','./app3.js','./app4.js','./app6.js','./manifest.webmanifest','./chip_in_logo_TRUE_TRANSPARENT.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
-  const url=new URL(e.request.url);
-  if(url.origin!==self.location.origin){ e.respondWith(fetch(e.request)); return; }
-  e.respondWith(fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    if(new URL(e.request.url).origin===self.location.origin){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+    return resp;
+  }).catch(()=>caches.match('./index.html'))));
 });
