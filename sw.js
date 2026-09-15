be91d84d069d6159b62316aceb6270b0690a864b
ChipButt <jameschipbutt@hotmail.com>
2026-09-15 13:14:11 +0100

    Refresh PWA cache for passphrase update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chipin-hq-v3';
-const ASSETS=['./','./index.html','./styles.css','./app1.js','./app2_1.js','./app2_2.js','./app2_3.js','./app2_4.js','./app3.js','./app4.js','./app6.js','./manifest.webmanifest','./chip_in_logo_TRUE_TRANSPARENT.png'];
+const CACHE='chipin-hq-v4';
+const ASSETS=['./','./index.html','./styles.css','./app1.js','./app2_1.js','./app2_2.js','./app2_3.js','./app2_4.js','./app3.js','./app4.js','./app6.js','./app7.js','./manifest.webmanifest','./chip_in_logo_TRUE_TRANSPARENT.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
